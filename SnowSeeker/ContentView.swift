8a8a9b40e2481ad03bbe804a77115eb56c250bd0
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-24 17:22:59 -0300

    Working with two side by side views

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            NavigationLink {
+                Text("New secondary")
+            } label: {
+                Text("Hello World")
+            }
+                .navigationTitle("Primary")
+            
+            Text("Secondary")
+            
+            Text("Terceira")
         }
-        .padding()
     }
 }
 
