ae78af3d8b7ac3dc3319b2140ced76eb6a85eea8
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-24 18:58:33 -0300

    Using Group as transparent container

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,23 +7,34 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Orlando Gabriel")
+            Text("Country: Brazil")
+            Text("Pet: Nikita")
+        }
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isPresented = false
+    @State private var layoutVertically = false
     
     var body: some View {
-        Text("Hello World")
-            .onTapGesture {
-                selectedUser = User()
-                isPresented = true
+        Group{
+            if layoutVertically {
+                VStack{
+                    UserView()
+                }
+            } else {
+                HStack{
+                    UserView()
+                }
             }
-            .alert("Welcome", isPresented: $isPresented, presenting: selectedUser) { user in
-                Button(user.id) { }
-            }
+        }
+        .onTapGesture {
+            layoutVertically.toggle()
+        }
     }
 }
 
